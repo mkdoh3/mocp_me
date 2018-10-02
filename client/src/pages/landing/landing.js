da26628a2594d7b1b1b30d12b2950da91edf9b21
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-10-02 15:46:26 -0500

    temp removed everything history, oauth, admin related. rewrote landing.js

--- client/src/pages/landing/landing.js
+++ client/src/pages/landing/landing.js
@@ -1,169 +1,168 @@
-import React, { ReactDOM, Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Dropzone from "react-dropzone";
-import Waypoint from 'react-waypoint';
-import { Grid, Row, Col, Container, Clearfix } from "react-grid-system"
+import Waypoint from "react-waypoint";
+import { Row, Col } from "react-grid-system";
 import axios from "axios";
+import autoBind from "auto-bind";
 
 import Logo from "../../components/logo/logo";
 import Phone from "../../components/phone/phone";
 import NavPanel from "../../components/nav_panel/nav_panel";
 
 class Landing extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onDrop = this.onDrop.bind(this);
-        this.onDragEnter = this.onDragEnter.bind(this);
-        this.onDragLeave = this.onDragLeave.bind(this);
-        this.handleTriggerOneEnter = this.handleTriggerOneEnter.bind(this);
-        this.handleTriggerOneLeave = this.handleTriggerOneLeave.bind(this);  
-        this.handleTriggerTwoEnter = this.handleTriggerTwoEnter.bind(this);
-        this.handleTriggerTwoLeave = this.handleTriggerTwoLeave.bind(this);
-        this.handleTriggerThreeEnter = this.handleTriggerThreeEnter.bind(this);
-        this.handleTriggerThreeLeave = this.handleTriggerThreeLeave.bind(this);
+    autoBind(this);
 
-        this.state = {
-            dropzoneActive: false,
-            rejectedFile: false,
-            triggerOne: false,
-            triggerTwo: false,
-            triggerThree: false 
-        }
-    }
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-    }
-    
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+    this.state = {
+      dropzoneActive: false,
+      fileRejected: false,
+      animationTriggerOne: false,
+      animationTriggerTwo: false,
+      animationTriggerThree: false
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.handleWindowResize);
+  }
 
-    onDragEnter() {
-        this.setState({ dropzoneActive: true });
-    }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize);
+  }
 
-    onDragLeave() {
-            this.setState({ dropzoneActive: false });
-    }
+  //reset animations when user changes browser window size, especially useful when switching between the media breakpoint
+  handleWindowResize() {
+    this.setState({
+      animationTriggerOne: false,
+      animationTriggerTwo: false,
+      animationTriggerThree: false
+    });
+  }
 
-    onDrop(accepted, rejected) {
-        if(rejected.length > 0) {
-            this.setState({ fileRejected: true });
-            return;
-        } else {
-            this.setState({ 
-                dropzoneActive: false,
-                fileUploaded: true,
-             });
-        }
+  onDragEnter() {
+    this.setState({ dropzoneActive: true });
+  }
 
-        const image = new FormData();
-        image.append("image", accepted[0]);
+  onDragLeave() {
+    this.setState({ dropzoneActive: false });
+  }
 
-        axios
-            .post("/api/upload", image)
-            .then(res => {
-                sessionStorage.setItem("uploadedImg", JSON.stringify(res.data.imageUrl));
-                this.props.history.push("/vision_search");})
-            .catch(err => console.log(err));
+  handleOnDrop(accepted, rejected) {
+    if (rejected.length > 0) {
+      console.log(rejected);
+      this.setState({ fileRejected: true });
+      return;
+    } else {
+      this.setState({
+        dropzoneActive: false,
+        fileUploaded: true
+      });
     }
 
-    handleTriggerOneEnter() {
-        this.setState({ triggerOne: true });
-    }
-    handleTriggerOneLeave() {
-        this.setState({ triggerOne: false });
-    }
+    const image = new FormData();
+    image.append("image", accepted[0]);
 
-    handleTriggerTwoEnter() {
-        this.setState({ triggerTwo: true });
-    }
-    handleTriggerTwoLeave() {
-        this.setState({ triggerTwo: false });
-    }
-
-    handleTriggerThreeEnter() {
-        this.setState({ triggerThree: true });
-    }
-    handleTriggerThreeLeave() {
-        this.setState({ triggerThree: false });
-    }
-
-    render() {
-
-        const { dropzoneActive } = this.state;
-
-        const overlayStyle = {
-            position: "fixed",
-            top: 0,
-            left: 0,
-            height:"100vh",
-            width:"100vw",
-            background: "rgba(100,100,100,0.1)"
-        };
-
-        return (
-            <div className="landingWrapper">
-                <Row>
-                    <Col xs={12} sm={7} md={7} className="phoneStyle">
-                        <Waypoint 
-                            onLeave={ this.handleTriggerOneLeave } 
-                            onEnter={ this.handleTriggerOneEnter } 
-                            scrollableAncestor={ window } 
-                            fireOnRapidScroll={ true } 
-                            topOffset="60%"/>
-                        <Logo />
-                        <Phone trigger={ this.state.triggerOne } />
-                    </Col>
-                    <Col xs={ 12 } sm={ 5 } md={ 5 }>
-                        <Dropzone
-                            style={{ position: "relative" }}
-                            multiple={ false }
-                            accept="image/*"
-                            maxSize={ 4000000 }
-                            minSize={ 5000 }
-                            onDrop={ this.onDrop }
-                            onDragEnter={ this.onDragEnter }
-                            onDragLeave={ this.onDragLeave }>
-                            { dropzoneActive && <div style={ overlayStyle }></div> }
-                            <div className="navPanel_1">
-                                <Waypoint   
-                                    onEnter={ this.handleTriggerTwoEnter }  
-                                    scrollableAncestor={ window } 
-                                    topOffset="70%" 
-                                    fireOnRapidScroll={ true }/>
-                                <NavPanel
-                                    text1="Upload your image"
-                                    text2="to connect to"
-                                    text3="the collection."
-                                    trigger={ this.state.triggerTwo }/>
-                                <button className="button"></button>
-                                {this.state.rejectedFile && <p><b>Please upload an image file between 5kb and 4mb in size</b></p>}
-                            </div>
-                        </Dropzone>
-                        <div className="navPanel_2">
-                            <Waypoint   
-                                onEnter={ this.handleTriggerThreeEnter } 
-                                scrollableAncestor={ window } 
-                                topOffset="80%"
-                                fireOnRapidScroll={ true }/>
-                            <NavPanel
-                                text1="Search our tags"
-                                text2="& add some more"
-                                text3="on the go!"
-                                trigger={ this.state.triggerThree }/>
-                            <Link to="/explore">
-                                <button className="button">
-                                    explore
-                                </button>
-                            </Link>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
+    axios
+      .post("/api/upload", image)
+      .then(res => {
+        sessionStorage.setItem(
+          "uploadedImg",
+          JSON.stringify(res.data.imageUrl)
         );
+        this.props.history.push("/vision_search");
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleAnimationTrigger(trigger) {
+    if (window.innerWidth < 576 && !this.state[trigger]) {
+      this.setState({ [trigger]: true });
     }
+  }
+
+  render() {
+    const { dropzoneActive } = this.state;
+
+    const overlayStyle = {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      height: "100vh",
+      width: "100vw",
+      background: "rgba(100,100,100,0.1)"
+    };
+
+    return (
+      <div className="landingWrapper">
+        <Row>
+          <Col xs={12} sm={7} md={7} className="phoneStyle">
+            <Waypoint
+              onLeave={() => this.handleAnimationTrigger("animationTriggerOne")}
+            />
+            <Logo />
+            <Phone animate={this.state.animationTriggerOne} />
+          </Col>
+          <Col xs={12} sm={5} md={5}>
+            <Dropzone
+              style={{ position: "relative" }}
+              multiple={false}
+              accept="image/*"
+              maxSize={4000000}
+              minSize={5000}
+              onDrop={this.handleOnDrop}
+              onDragEnter={this.onDragEnter}
+              onDragLeave={this.onDragLeave}
+            >
+              {dropzoneActive && <div style={overlayStyle} />}
+              <div className="navPanel_1">
+                <Waypoint
+                  onEnter={() =>
+                    this.handleAnimationTrigger("animationTriggerTwo")
+                  }
+                  bottomOffset="55%"
+                />
+                <NavPanel
+                  text1="Upload your image"
+                  text2="to connect to"
+                  text3="the collection."
+                  animate={this.state.animationTriggerTwo}
+                />
+                <button className="button">
+                  {this.state.fileUploaded ? "processing..." : "add image"}
+                </button>
+                {this.state.fileRejected && (
+                  <p>
+                    <b>
+                      Please upload an image file between 5kb and 4mb in size
+                    </b>
+                  </p>
+                )}
+              </div>
+            </Dropzone>
+            <div className="navPanel_2">
+              <Waypoint
+                onEnter={() =>
+                  this.handleAnimationTrigger("animationTriggerThree")
+                }
+                bottomOffset="55%"
+              />
+              <NavPanel
+                text1="Search our tags"
+                text2="& add some more"
+                text3="on the go!"
+                animate={this.state.animationTriggerThree}
+              />
+              <Link to="/explore">
+                <button className="button">explore</button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
-export default withRouter(Landing);
+export default Landing;
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,34 @@
-const routes = (function(){
+const routes = (function() {
+  // Determine environment
+  const env = process.env.NODE_ENV || "development";
 
-	// Determine environment
-	const env = process.env.NODE_ENV || "development";
+  // Dependencies
+  const apiRoutes = require("./api-routes");
+  const path = require("path");
+  const bodyParser = require("body-parser");
 
-	// Dependencies
-	const adminRoutes = require("./admin-routes");
-	const apiRoutes = require("./api-routes");
-	const path = require("path");
-	const bodyParser = require("body-parser");
+  // Instantiate express router
+  const router = require("express").Router();
 
-	// Instantiate express router
-	const router = require("express").Router();
+  // Middleware
+  router.use(bodyParser.urlencoded({ extended: false }));
+  router.use(bodyParser.json());
 
-	// Middleware
-	router.use(bodyParser.urlencoded({extended: false}));
-	router.use(bodyParser.json());
+  // API calls are directed here
+  router.use("/api", apiRoutes);
 
-	// API calls are directed here
-	router.use("/admin-api", adminRoutes);
-	router.use("/api", apiRoutes);
+  // All other paths return React app, including /admin
+  router.get("*", function(req, res) {
+    let filePath;
+    if (env === "development") {
+      filePath = "../client/public/index.html";
+    } else {
+      filePath = "../client/build/index.html";
+    }
+    res.sendFile(path.join(__dirname, filePath));
+  });
 
-	// All other paths return React app, including /admin
-	router.get("*", function (req, res){
-		let filePath;
-		if (env === "development"){
-			filePath = "../client/public/index.html";
-		} else {
-			filePath = "../client/build/index.html";
-		}
-		res.sendFile(path.join(__dirname, filePath));
-	});
-
-	return router;
-
+  return router;
 })();
 
 module.exports = routes;
