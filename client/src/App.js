ce5a994594edd7ad33b6e0a6d7cc2ae4cf52004c
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-10-02 15:56:58 -0500

    react router fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Normalizes CSS defaults in varying browsers
 import { Normalize } from "normalize-css";
 
