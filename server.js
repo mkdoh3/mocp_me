d2ca704a510c44801a562bf80e70bb9ce4cc32e7
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-10-02 14:11:25 -0500

    cleaned and auditted packages

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 // Clears terminal window
-process.stdout.write('\033c');
+process.stdout.write("\033c");
 
 // Dependencies
-const express = require ("express");
-const bodyParser = require ("body-parser");
-const routes = require ("./routes");
-var path = require('path');
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+var path = require("path");
 
 // Set Port number to environmental variable -or- Port 3001
 const PORT = process.env.PORT || 3001;
@@ -17,15 +15,16 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(routes);
 
 // Activate express server
-app.listen(PORT, function(){
-	console.log(`
+app.listen(PORT, function() {
+  console.log(`
 
 🍑💨 Please, pay no attention to the express app listening on port ${PORT}.`);
 });
